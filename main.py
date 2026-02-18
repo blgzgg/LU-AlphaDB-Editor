4b57eb42e818b06aa232bcffd6a6e0f91877ef60
blgzgg <brian.grassi456@gmail.com>
2026-02-18 06:09:04 -0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+from tkinter import filedialog as fd
+
+# Tkinter configuration
+root = tk.Tk()
+
+root.title("Lego Universe AlphaDB Editor")
+root.configure(background="yellow")
+root.minsize(200, 200)
+root.maxsize(500, 500)
+root.geometry("500x500+50+50")
+
+
+# Functions
+def select_db_path():
+    filename = fd.askopenfilename()
+    print(filename)
+
+
+# GUI Elements
+db_path_button = tk.Button(root, text="Select path to ivantest.xml", command=select_db_path)
+db_path_button.pack()
+
+tk.Label(root, text="Select path to ").pack()
+tk.Label(root, text="- Maya Angelou").pack()
+
+
+
+root.mainloop()
+
+
